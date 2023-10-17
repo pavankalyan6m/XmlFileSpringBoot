f2c91b4c383ae307f5e786b1aa352a8baddd149f
PavanKalyan <pavankalyan6md@gmail.com>
2023-10-17 11:36:42 +0530

    XML Project

--- src/main/java/com/example/FileReader/Controller/XmlFileController.java
+++ src/main/java/com/example/FileReader/Controller/XmlFileController.java
@@ -1,15 +1,11 @@
 package com.example.FileReader.Controller;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
 
 @RestController
 class XmlFileController {
@@ -62,4 +58,45 @@ class XmlFileController {
             return "Failed to write the XML content to the target file.";
         }
     }
+
+        @GetMapping("/read-xml-folder")
+        public String readXmlFilesInFolder(@RequestParam String folderPath) {
+            File folder = new File(folderPath);
+            if (!folder.exists() || !folder.isDirectory()) {
+                return "Invalid folder path.";
+            }
+
+            File[] xmlFiles = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".xml"));
+
+            if (xmlFiles == null || xmlFiles.length == 0) {
+                return "No XML files found in the specified folder.";
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            for (File xmlFile : xmlFiles) {
+                try {
+                    // Read the XML file
+                    BufferedReader bufferedReader = new BufferedReader(new FileReader(xmlFile));
+                    String line;
+                    StringBuilder xmlContent = new StringBuilder();
+
+                    while ((line = bufferedReader.readLine()) != null) {
+                        xmlContent.append(line).append("\n");
+                    }
+
+                    bufferedReader.close();
+
+                    // Print the XML content to the console
+                    System.out.println("XML Content from " + xmlFile.getName() + ":");
+                    System.out.println(xmlContent.toString());
+
+                    result.append("XML content from ").append(xmlFile.getName()).append(" printed to the console. Check your application's logs.\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    result.append("Failed to read and print the XML file ").append(xmlFile.getName()).append(".\n");
+                }
+            }
+            return result.toString();
+        }
 }
